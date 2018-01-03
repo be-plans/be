4d38e34e79156bd2a291266b5f62a52ee997f7c2
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-04 01:53:23 +0200

    Update CMake to 3.10.1

--- cmake/plan.ps1
+++ cmake/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="cmake"
 $pkg_origin="core"
-$base_version="3.6"
-$pkg_version="$base_version.0"
+$base_version="3.10"
+$pkg_version="$base_version.1"
 $pkg_description="CMake is an open-source, cross-platform family of tools designed to build, test and package software"
 $pkg_upstream_url="https://cmake.org/"
 $pkg_license=@("BSD-3-Clause")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="http://cmake.org/files/v$base_version/cmake-$pkg_version-win64-x64.msi"
-$pkg_shasum="ab491f3d925a2251be04752d16a81bcaf90b88abb48ac8b5760fb090d540b11f"
+$pkg_shasum="23fe84a5cec997e1b8a7725713cf8b0934071f5b25b53c51acf51490dff4e468"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
--- cmake/plan.sh
+++ cmake/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=cmake
 pkg_origin=core
-_base_version=3.8
-pkg_version=${_base_version}.2
+_base_version=3.10
+pkg_version=${_base_version}.1
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('BSD-3-Clause')
 pkg_description="CMake is an open-source, cross-platform family of tools designed to build, test and package software"
 pkg_upstream_url="https://cmake.org/"
 pkg_source="https://cmake.org/files/v${_base_version}/cmake-${pkg_version}.tar.gz"
-pkg_shasum=da3072794eb4c09f2d782fcee043847b99bb4cf8d4573978d9b2024214d6e92d
+pkg_shasum=7be36ee24b0f5928251b644d29f5ff268330a916944ef4a75e23ba01e7573284
 pkg_deps=(
   core/glibc
   core/gcc-libs
@@ -29,8 +29,7 @@ pkg_bin_dirs=(bin)
 source ../defaults.sh
 
 do_build() {
-  ./bootstrap
-  ./configure --prefix="${pkg_prefix}"
+  ./bootstrap --prefix="${pkg_prefix}"
   make -j "$(nproc)"
 }
 
