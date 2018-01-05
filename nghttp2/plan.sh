354e7970986396ebc1932091dce1f06494ba289d
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-05 22:12:51 +0200

    Update nghttp2 to 1.29.0

--- nghttp2/plan.sh
+++ nghttp2/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nghttp2
 pkg_origin=core
-pkg_version=1.24.0
+pkg_version=1.29.0
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
-pkg_source=https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=5058da99c94764ff39f4a7046b9c8f0b6bafa10ec2fc096945a5d9d693654840
+pkg_source=https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.xz
+pkg_shasum=a7a1b18be57be6a53a7739988ea27d6ec9209e7b0e8372b8483cd911d7838739
 pkg_dirname=${pkg_name}-${pkg_version}
 pkg_build_deps=(
   be/make
@@ -14,3 +14,5 @@ pkg_build_deps=(
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
+
+source ../defaults.sh
