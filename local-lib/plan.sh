0f72a1769c77bdef4748ce748cbb9a8db23fad0f
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-05 22:13:22 +0200

    Update local-lib to 2.000024

--- local-lib/plan.sh
+++ local-lib/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=local-lib
-pkg_version=2.000023
+pkg_version=2.000024
 pkg_origin=core
 pkg_license=('Artistic-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
@@ -8,10 +8,15 @@ pkg_upstream_url='https://github.com/Perl-Toolchain-Gang/local-lib'
 pkg_source=http://search.cpan.org/CPAN/authors/id/H/HA/HAARG/${pkg_name}-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_dirname=${pkg_name}-${pkg_version}
-pkg_shasum=0cf6f5916fc5ae86ef30c5df87414fd3587eff40642bc85ab17a59047bb82099
-pkg_deps=(core/glibc be/perl)
+pkg_shasum=2e9b917bd48a0615e42633b2a327494e04610d8f710765b9493d306cead98a05
+pkg_deps=(
+  core/glibc
+  be/perl
+)
 pkg_build_deps=(
-  be/gcc be/make be/coreutils
+  be/gcc
+  be/make
+  be/coreutils
   be/perl
 )
 pkg_lib_dirs=(lib)
