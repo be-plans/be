71d9cfe80aab21a2745f6effa9846d3100088da3
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-06 00:13:16 +0200

    Update dbus to 1.12.2

--- dbus/plan.sh
+++ dbus/plan.sh
@@ -1,16 +1,17 @@
 pkg_name=dbus
 pkg_origin=core
-pkg_version="1.10.18"
+pkg_version=1.12.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPLv2')
 pkg_description="D-Bus is a message bus system, a simple way for applications to talk to one another."
 pkg_upstream_url="https://www.freedesktop.org/wiki/Software/dbus/"
 pkg_source="https://dbus.freedesktop.org/releases/dbus/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="6049ddd5f3f3e2618f615f1faeda0a115104423a7996b7aa73e2f36e38cc514a"
+pkg_shasum=272bb5091770b047c8188b926d5e6038fa4fe6745488b2add96b23e2d9a83d88
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   be/autoconf
   be/automake
+  be/pkg-config
   be/make
   be/gcc
   be/expat
