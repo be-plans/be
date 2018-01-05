b276ce3349a18fb4aa37d0d2ee8603937423a0c3
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-05 22:16:06 +0200

    Update libmpc to 1.1.0-rc(mpfr 4.0.0 compat)

--- libmpc/plan.sh
+++ libmpc/plan.sh
@@ -1,17 +1,31 @@
 pkg_origin=core
 pkg_name=libmpc
 pkg_distname=mpc
-pkg_version=1.0.3
+pkg_version=1.1.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.0')
-pkg_source=http://www.multiprecision.org/mpc/download/${pkg_distname}-${pkg_version}.tar.gz
-pkg_shasum=617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3
+### Switch back the source after the official 1.1.0 release(mpfr 4.0 compat)
+pkg_source="https://scm.gforge.inria.fr/anonscm/gitweb?p=mpc/mpc.git;a=snapshot;h=608a22178bea8fe9bebab88b5bc23a914f0926da;sf=tgz"
+pkg_shasum=6397d8cd3d52ec2a4dde49a8f64cd1d8f0267e1a0fc695e2bdfee4c05b5dd55c
+pkg_dirname=${pkg_name}-${pkg_version}
+### end
+# pkg_source=http://www.multiprecision.org/mpc/download/${pkg_distname}-${pkg_version}.tar.gz
+# pkg_shasum=617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3
 pkg_deps=(
   core/glibc
   be/gmp
-  lilian/mpfr
+  be/mpfr
 )
 pkg_build_deps=(
+  ### Remove autoconf stuff after official 1.1.0 release
+  be/autoconf
+  be/automake
+  be/pkg-config
+  be/libtool
+  be/texinfo
+  be/m4
+  be/git
+  ### end
   be/coreutils
   be/diffutils
   be/patch
@@ -26,11 +40,28 @@ pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_disabled_features=(pic)
 source ../defaults.sh
 
+do_unpack() {
+  mkdir -p "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname" > /dev/null
+    tar xf "$HAB_CACHE_SRC_PATH/$pkg_filename" --strip 1 --no-same-owner
+  popd > /dev/null
+}
+
 do_prepare() {
   do_default_prepare
 
-  LDFLAGS="$LDFLAGS -Wl,-rpath=${LD_RUN_PATH},--enable-new-dtags"
+  export LDFLAGS="$LDFLAGS -Wl,-rpath=${LD_RUN_PATH},--enable-new-dtags"
   build_line "Updating LDFLAGS=$LDFLAGS"
+}
+
+do_build() {
+  autoreconf --install --force --verbose
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --with-gmp="$(pkg_path_for gmp)" \
+    --with-mpfr="$(pkg_path_for mpfr)"
+
+  make -j "$(nproc)"
 }
 
 do_check() {
