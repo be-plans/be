99bf330338c784fc355ea426f6ec4bddfe807ac8
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-05 22:12:20 +0200

    Update openssh to 7.6p1

--- openssh/plan.sh
+++ openssh/plan.sh
@@ -1,24 +1,30 @@
 pkg_origin=core
 pkg_name=openssh
-pkg_version=7.5p1
+pkg_version=7.6p1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Provides OpenSSH client and server."
 pkg_license=('BSD')
 pkg_source=http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=9846e3c5fab9f0547400b4d2c017992f914222b3fd1f8eee6c7dc6bc5e59f9f0
+pkg_shasum=a323caeeddfe145baaa0db16e98d784b1fbc7dd436a6bf1f479dfd5cd1d21723
 pkg_upstream_url=https://www.openssh.com/
 pkg_bin_dirs=(bin sbin libexec)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_deps=(
-  core/glibc be/openssl be/zlib
+  core/glibc
+  be/openssl
+  be/zlib
 )
 pkg_build_deps=(
-  be/coreutils be/gcc be/make
+  be/coreutils
+  be/gcc
+  be/make
 )
 
 pkg_svc_user="root"
 pkg_svc_group="root"
+
+source ../defaults.sh
 
 do_build() {
   ./configure --prefix="${pkg_prefix}" \
