785f0bdbe9836bcfd37ae0cbe4218eecd08fa853
Lilian A. Moraru <lilian.moraru90@gmail.com>
2018-01-18 21:38:48 +0200

    Update Git to 2.16.0

--- git/plan.sh
+++ git/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=git
-pkg_version=2.15.1
+pkg_version=2.16.0
 pkg_origin=core
 pkg_description="Git is a free and open source distributed version control
   system designed to handle everything from small to very large projects with
@@ -9,7 +9,7 @@ pkg_license=('GPL-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://www.kernel.org/pub/software/scm/git/${pkg_name}-${pkg_version}.tar.xz
 pkg_filename=${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=999c90fd7d45066992cdb87dda35bdff6dfc1d01496118ea718dfb866da4045c
+pkg_shasum=0d10764e66b3d650dee0d99a1c77afa4aaae5e739c0973fcc1c5b9e6516e30f8
 pkg_deps=(
   be/cacerts
   be/curl
